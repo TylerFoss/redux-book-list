664dbad6efc331465d6cbd14516feede429e3f66
Tyler Foss <tylerscottfoss@gmail.com>
2017-04-16 23:02:45 -0600

    book list using reducers and actions

---
+++ src/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import BooksReducer from "./reducer_books";
+import ActiveBook from "./reducer_active_book";
+// a reducer is a function that returns a peice
+// of the application state
+
+const rootReducer = combineReducers({
+  books: BooksReducer,
+  activeBook: ActiveBook
+});
+
+export default rootReducer;
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+  return[
+    {title: "Book1", pages: 101},
+    {title: "Book2", pages: 102},
+    {title: "Book3", pages: 103},
+    {title: "Book4", pages: 104}
+  ]
+}
